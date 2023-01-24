688e89b3f3c070cc5859115e0ae5c9b6d9e1cf06
Katie Wilson <kmcwilson4@gmail.com>
2023-01-23 19:16:40 -0500

    changed the package.json

--- src/index.js
+++ src/index.js
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import HashRouter from 'react-router-dom';
 
 
 ReactDOM.render(
-  <HashRouter>
-    <App />
-    </HashRouter>,
+    <App />,
   document.getElementById('root')
 );
 
